cea99ee2fd8d1cc32d5eecbe118d1051748f08eb
arif <moarifur@gmail.com>
2021-08-10 14:58:37 +0600

    Express Based BicycleShop app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bicycles = require('./data/data.json')
+
+app.get('/', (req, res) => {
+    return res.send(bicycles)
+})
+app.get('/bicycle', (req, res) => {
+    const bicycle = bicycles.find(b => b.id === req.query.id)
+    return res.send(bicycle)
+})
+app.listen(4000, () => console.log('Running!'))
